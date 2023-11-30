5ed59a8acaf4e67bdeac8a756575b2d260353a70
AndersonFerrer <ferrer.280104@gmail.com>
2023-11-30 16:23:34 -0500

    fix database

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,12 +1,17 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize(
-  "tasks", //dbName
-  "postgres", //userDB
-  "Gej9eqb8*!", //passwordDb
-  {
-    host: "localhost",
-    port: 2801,
-    dialect: "postgres",
-  }
-);
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: "ep-polished-pine-11135275-pooler.us-east-1.postgres.vercel-storage.com",
+  port: 5432, // Puerto por defecto de PostgreSQL
+  username: "default",
+  password: "lbLB1VM9xmDs",
+  database: "verceldb",
+  ssl: true, // Render generalmente requiere SSL para conexiones a bases de datos
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Si estás teniendo problemas con certificados SSL, esto puede ser necesario
+    },
+  },
+});
